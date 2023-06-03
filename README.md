87b2b5fd479445f7f1ea39368e51f6c0f464bee7
Ze Yu <angzeyu@gmail.com>
2023-06-03 13:21:47 +0800

    Automate S3 deploy and ECR image upload

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # AppTracky
 
-WIP Job application tracking demo app made mostly to familiarise with OAuth authentication flows on Spring Boot.
+Job application tracking demo app made mostly to:
+- Familiarise with OAuth authentication flows on Spring Boot.
+- AWS practice (ECS, ECR, IAM, S3, S3 presigning, RDS, SSM Parameter store)
+- Try my hands at browser drag-drop APIs
 
 Done:
 - [x] Authentication flows (APIs / frontend)
@@ -12,17 +15,29 @@ Done:
    - [x] Authentication related things
 - [x] Drag-and-drop kanban board for applications and APIs
 - [x] Presigned urls S3 uploads for PDF resumes
-- [x] AWS SSM Parameter Store integration for OAuth secrets
+- [x] AWS SSM Parameter Store integration for OAuth/RDS secrets
 - [x] Containerize the backend for easier development, and ECS deployment
+- [x] Setup ECS task defs, cluster, S3 bucket policies, IAM for GH actions
+- [x] Build and deploy image to ECS, and frontend to S3 automatically with GH actions
 
 TBD:
-- [ ] Deploy to ECS (IAM roles, maybe CF template, ..., CI/CD with gh actions)
+- [ ] ($$) (Maybe) HTTPs and domains for S3 frontend and ALB to front ECS (instead of dynamic public IP)
+- [ ] (Maybe) CloudFormation-ify everything AWS related
 
 The backend is built with Spring Boot, and heavy use of Spring Security for Authentication.
 
 Both basic auth and oauth flows ultimately result in the issuing of a JWT used for endpoint authentication and authorization. 
 
 Frontend is a simple Vue app built on Vuetify + Vite.
+
+
+## URLS
+
+S3 Frontend: http://zyang-apptracky.s3-website-ap-northeast-1.amazonaws.com/
+
+ECS Backend: (dynamic IP) (currently taken offline to save $$)
+
+## Screenshots
 
 ![](./screenshots/ss6.png)
 
--- backend/src/main/java/com/demo/apptracky/config/SsmParameterStoreConfig.java
+++ backend/src/main/java/com/demo/apptracky/config/SsmParameterStoreConfig.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.services.ssm.SsmClient;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathRequest;
 import software.amazon.awssdk.services.ssm.model.GetParametersByPathResponse;
 
+import java.util.Arrays;
 import java.util.Properties;
 
 public class SsmParameterStoreConfig implements ApplicationListener<ApplicationEnvironmentPreparedEvent> {
@@ -15,11 +16,17 @@ public class SsmParameterStoreConfig implements ApplicationListener<ApplicationE
 
     private static final String CLIENT_ID_SECRET = "spring.security.oauth2.client.registration.google.client-secret";
 
+    private static final String JDBC_URL = "spring.datasource.url";
+
+    private static final String JDBC_USER = "spring.datasource.username";
+
+    private static final String JDBC_PW = "spring.datasource.password";
+
     @Override
     public void onApplicationEvent(final ApplicationEnvironmentPreparedEvent event) {
-        final GetParametersByPathResponse response;
+        final GetParametersByPathResponse oauthParameters;
         try (final SsmClient ssmClient = SsmClient.builder().build()) {
-            response = ssmClient.getParametersByPath(
+            oauthParameters = ssmClient.getParametersByPath(
                     GetParametersByPathRequest.builder()
                             .path("/apptracky/oauth/google/")
                             .withDecryption(true)
@@ -27,12 +34,12 @@ public class SsmParameterStoreConfig implements ApplicationListener<ApplicationE
             );
         }
 
-        final String clientId = response.parameters().stream()
+        final String clientId = oauthParameters.parameters().stream()
                 .filter(p -> p.name().endsWith("client-id"))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Missing Google oauth client id"))
                 .value();
-        final String clientSecret = response.parameters().stream()
+        final String clientSecret = oauthParameters.parameters().stream()
                 .filter(p -> p.name().endsWith("client-secret"))
                 .findFirst()
                 .orElseThrow(() -> new RuntimeException("Missing Google oauth client secret"))
@@ -40,6 +47,37 @@ public class SsmParameterStoreConfig implements ApplicationListener<ApplicationE
 
         ConfigurableEnvironment environment = event.getEnvironment();
         Properties props = new Properties();
+
+        if (Arrays.asList(environment.getActiveProfiles()).contains("prod")) {
+            final GetParametersByPathResponse rdsParameters;
+            try (final SsmClient ssmClient = SsmClient.builder().build()) {
+                rdsParameters = ssmClient.getParametersByPath(
+                        GetParametersByPathRequest.builder()
+                                .path("/apptracky/rds/")
+                                .withDecryption(true)
+                                .build()
+                );
+            }
+            final String rdsEndpoint = rdsParameters.parameters().stream()
+                    .filter(p -> p.name().endsWith("url"))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Missing RDS Endpoint"))
+                    .value();
+            final String rdsUser = rdsParameters.parameters().stream()
+                    .filter(p -> p.name().endsWith("user"))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Missing RDS User"))
+                    .value();
+            final String rdsPw = rdsParameters.parameters().stream()
+                    .filter(p -> p.name().endsWith("pw"))
+                    .findFirst()
+                    .orElseThrow(() -> new RuntimeException("Missing RDS Pw"))
+                    .value();
+            props.put(JDBC_URL, rdsEndpoint);
+            props.put(JDBC_USER, rdsUser);
+            props.put(JDBC_PW, rdsPw);
+        }
+
         props.put(CLIENT_ID_KEY, clientId);
         props.put(CLIENT_ID_SECRET, clientSecret);
         environment.getPropertySources().addFirst(new PropertiesPropertySource("oauth", props));
